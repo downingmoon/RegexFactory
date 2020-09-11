1918b3e65a98d543a650ba258c62307d17a60901
downingmoon <joy97q12@naver.com>
2020-09-11 21:30:15 +0900

    INIT

---
+++ RegexFactory.js
@@ -0,0 +1,15 @@
+'use strict'
+/**
+ * Javascript Regular Expression Factory
+ * Author: DowningMoon
+ * 2020.09.11
+ * @see https://github.com/downingmoon
+ */
+class RegexFactory {
+    static HP_TYPE_1 = "";
+    constructor () {}
+
+    test() {
+        console.log("TEST")
+    }
+}
